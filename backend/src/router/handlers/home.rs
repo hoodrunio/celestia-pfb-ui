c4f8d01932253ef4d49820cac2cdd049adff1241
amorf <fatihermetin@gmail.com>
2023-05-03 19:10:01 +0300

    Add home response

--- backend/src/router/handlers/home.rs
+++ backend/src/router/handlers/home.rs
@@ -1,3 +1,23 @@
-pub async fn home() -> &'static str {
-    "Hello, World!"
+use axum::Json;
+use serde::{Deserialize, Serialize};
+
+use crate::{res, AppError, AppResponse};
+
+use super::PfbGeneratedTxDataResponse;
+
+pub async fn home() -> Result<Json<AppResponse<HomeResponse>>, AppError> {
+    let data = HomeResponse::new_with_pfb_tx_data();
+    Ok(res(data))
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct HomeResponse {
+    pub pfb_tx_data: PfbGeneratedTxDataResponse,
+}
+
+impl HomeResponse {
+    pub fn new_with_pfb_tx_data() -> Self {
+        let pfb_tx_data = PfbGeneratedTxDataResponse::new(None, None);
+        Self { pfb_tx_data }
+    }
 }
--- backend/src/router/handlers/mod.rs
+++ backend/src/router/handlers/mod.rs
@@ -1,7 +1,9 @@
+mod generator;
 mod home;
 mod pfb;
 mod utils;
 
+pub use generator::*;
 pub use home::*;
 pub use pfb::*;
 pub use utils::*;
