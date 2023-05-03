c0920f52dec70cfb8accfcfc80df56f86283ee0e
amorf <fatihermetin@gmail.com>
2023-05-03 21:46:28 +0300

    Refactor on submit

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,13 +1,121 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
+import { Box, Button, Grid } from '@mui/material';
+import AppInput from './components/AppInput';
+import { useForm } from 'react-hook-form';
+import { useCallback } from 'react';
+
+enum FORM_FIELD {
+  NAMESPACE = 'namespace_id',
+  MESSAGE = 'message',
+  NODE_URL = 'node_url',
+  PORT = 'port',
+}
+
+interface PfbFormType {
+  [FORM_FIELD.NAMESPACE]: '';
+  [FORM_FIELD.MESSAGE]: '';
+  [FORM_FIELD.NODE_URL]: '';
+  [FORM_FIELD.PORT]: '';
+}
 
 export function App() {
+  const form = useForm({ mode: 'onChange' });
+  const { handleSubmit, reset, formState } = form;
+  const { isValid: isFormValid, isDirty: isFormDirty } = formState;
+
+  const formInitValue = useCallback(
+    () => ({
+      [FORM_FIELD.NAMESPACE]: '',
+      [FORM_FIELD.MESSAGE]: '',
+      [FORM_FIELD.NODE_URL]: '',
+      [FORM_FIELD.PORT]: '',
+    }),
+    []
+  );
+  const onReset = useCallback(
+    () => reset(formInitValue()),
+    [reset, formInitValue]
+  );
+
+  const onSubmit = useCallback((data: any) => {
+    const formData = data as PfbFormType;
+  }, []);
+
   return (
-    <div>
-      <NxWelcome title="frontend" />
-    </div>
+    <form>
+      <Grid container spacing={1}>
+        <Grid
+          item
+          xs={12}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <AppInput
+            formName={FORM_FIELD.NAMESPACE}
+            form={form}
+            label="Namespace Id"
+            toolTipText="Random generated namespace Id"
+          />
+          <AppInput
+            formName={FORM_FIELD.MESSAGE}
+            form={form}
+            label="Message"
+            toolTipText="Random generated message"
+          />
+        </Grid>
+        <Grid
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          item
+          xs={12}
+        >
+          <AppInput
+            formName={FORM_FIELD.NODE_URL}
+            form={form}
+            label="Node Url"
+            toolTipText="Your node public url"
+          />
+          <AppInput
+            formName={FORM_FIELD.PORT}
+            form={form}
+            label="Port"
+            toolTipText="Your node public port"
+          />
+        </Grid>
+        <Grid
+          xs
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Box mx={1}>
+            <Button
+              variant="contained"
+              disabled={!isFormValid}
+              onClick={handleSubmit(onSubmit)}
+            >
+              Submit
+            </Button>
+          </Box>
+          <Box mx={1}>
+            <Button
+              variant="contained"
+              disabled={!isFormDirty}
+              onClick={onReset}
+            >
+              Reset
+            </Button>
+          </Box>
+          <Box mx={1}>
+            <Button variant="contained">Generate</Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </form>
   );
 }
 
