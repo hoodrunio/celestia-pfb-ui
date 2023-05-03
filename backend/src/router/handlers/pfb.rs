8a278373e4e844078e238c0f9f948011ae5626f8
amorf <fatihermetin@gmail.com>
2023-05-03 19:10:14 +0300

    Add pfb generated tx data response

--- backend/src/router/handlers/pfb.rs
+++ backend/src/router/handlers/pfb.rs
@@ -1,3 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+use super::gen_pfb_tx_data;
+
 pub async fn submit_pfb_transaction() -> &'static str {
     "Hello, World!"
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct PfbGeneratedTxDataResponse {
+    pub namespace_id: String,
+    pub message: String,
+}
+
+impl PfbGeneratedTxDataResponse {
+    pub fn new(namespace_id: Option<String>, message: Option<String>) -> Self {
+        let namespace_id = namespace_id.unwrap_or_else(|| gen_pfb_tx_data().namespace_id);
+        let message = message.unwrap_or_else(|| gen_pfb_tx_data().message);
+
+        Self {
+            namespace_id,
+            message,
+        }
+    }
 }
