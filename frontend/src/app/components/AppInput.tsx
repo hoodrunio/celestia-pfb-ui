4977c9a4f339577010e2a27793cab996daefc543
amorf <fatihermetin@gmail.com>
2023-05-03 21:38:32 +0300

    Add app text input

---
+++ frontend/src/app/components/AppInput.tsx
@@ -0,0 +1,66 @@
+import { Box, TextField } from '@mui/material';
+import { Controller, FieldValues, UseFormReturn } from 'react-hook-form';
+
+interface AppInputProps {
+  placeholder?: string;
+  label: string;
+  formName: string;
+  toolTipText?: string;
+  required?: boolean;
+  maxL?: number;
+  minL?: number;
+  form: UseFormReturn<FieldValues, any>;
+}
+
+const AppInput = (props: AppInputProps) => {
+  const {
+    formName,
+    placeholder,
+    label,
+    toolTipText,
+    form,
+    required = true,
+    minL = 0,
+    maxL = 100,
+  } = props;
+
+  return (
+    <Box py={1.5} mx={1}>
+      <Controller
+        name={formName}
+        defaultValue=""
+        control={form.control}
+        rules={{
+          required: {
+            value: required,
+            message: `Field is required`,
+          },
+          minLength: {
+            value: minL,
+            message: `Minimum length is ${minL}`,
+          },
+          maxLength: {
+            value: maxL,
+            message: `Maximum length is ${maxL}`,
+          },
+        }}
+        render={({ field, formState }) => {
+          const error = formState?.errors[formName];
+          const helperText = error?.message?.toString() ?? toolTipText;
+          return (
+            <TextField
+              placeholder={placeholder}
+              label={label}
+              required
+              helperText={helperText}
+              error={!!error}
+              {...field}
+            />
+          );
+        }}
+      />
+    </Box>
+  );
+};
+
+export default AppInput;
