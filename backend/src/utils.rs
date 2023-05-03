3460cb10f12378df18790d2d42431cb1bfe9ee68
amorf <fatihermetin@gmail.com>
2023-05-03 17:03:13 +0300

    Add any how utils

---
+++ backend/src/utils.rs
@@ -0,0 +1,25 @@
+use axum::response::{IntoResponse, Response};
+use reqwest::StatusCode;
+
+#[derive(Debug)]
+pub struct AppError(anyhow::Error);
+
+// Tell axum how to convert `AppError` into a response.
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
