d8147da1ce317290c205df0c31fac1d04d3f7751
amorf <fatihermetin@gmail.com>
2023-05-05 00:32:34 +0300

    Prevent referch on focus for generate data

--- frontend/src/app/hooks/apiHooks/useGeneratePfbParams.ts
+++ frontend/src/app/hooks/apiHooks/useGeneratePfbParams.ts
@@ -4,7 +4,8 @@ import useSWR from 'swr';
 export const useGetPbfTxData = (): FethcerHookReturn => {
   const { isLoading, data, error, mutate } = useSWR(
     { path: '/generate_pfb_tx_data' },
-    fetcher
+    fetcher,
+    { revalidateOnFocus: false }
   );
 
   return { isLoading, data, error, mutate };
