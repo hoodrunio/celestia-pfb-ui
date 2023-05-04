5262c6dd095e05d01f5efd3ea8ec53a1fbe46c09
amorf <fatihermetin@gmail.com>
2023-05-05 01:33:45 +0300

    Change listening port

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -23,7 +23,7 @@ async fn main() {
 
     tracing::info!("Starting server...");
     // run it with hyper on localhost:3000
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:9898".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
