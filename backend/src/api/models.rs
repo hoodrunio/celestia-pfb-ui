4c163a756ec1dd6ee5b0d84f3ab4a29cd03c9cdf
amorf <fatihermetin@gmail.com>
2023-05-06 01:00:25 +0300

    Fix mode field orders

--- backend/src/api/models.rs
+++ backend/src/api/models.rs
@@ -30,17 +30,17 @@ impl SubmitPfbNodeRequest {
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct SubmitPfbNodeResponse {
-    pub height: i64,
     pub txhash: Option<String>,
+    pub height: i64,
     pub codespace: Option<String>,
     pub code: Option<i64>,
-    #[serde(rename = "raw_log")]
-    pub raw_log: Option<String>,
-    pub logs: Option<Value>,
     #[serde(rename = "gas_wanted")]
     pub gas_wanted: Option<i64>,
     #[serde(rename = "gas_used")]
     pub gas_used: Option<i64>,
+    #[serde(rename = "raw_log")]
+    pub raw_log: Option<String>,
+    pub logs: Option<Value>,
     pub events: Vec<Event>,
 }
 
